36fb9b5950cbe5662aa211e53dc8fa997407768c
arosteguydaniel-creator <arosteguy.daniel@gmail.com>
2026-07-21 21:14:15 -0400

    Use PORT env variable for deployment

--- server.js
+++ server.js
@@ -780,7 +780,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
